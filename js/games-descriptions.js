e1c74b27fb074c3382e2415b50bc8bb7893d1e2f
al1-ce <al1-ce@list.ru>
2022-09-26 19:00:01 +0300

    initial commit

---
+++ js/games-descriptions.js
@@ -0,0 +1,143 @@
+var descriptionVolga = `
+**FIST WORLD VOLGA**
+
+**Release Date: April 30th 2021**
+
+**Platform: Itch.io**
+
+**The year is 2391.**
+
+Mankind has long
+expanded into the great starry void. War after war, its path across
+outer space was paved by the flesh and bone of the millions who have fallen. Even in times of peace, the echoes of battles to come ring in
+the ears of those who managed to survive, a grim reminder that all
+that lives is shackled to the unbreakable chains of history....
+
+**...but hey, at least we've got giant robot arenas now.**
+
+Scattered across
+known space under the ownership of Galactica Sports Entertainment,
+"Fist World" combat circuits are a staple of recreational
+mech combat, taking up to 15% of the whole battleframe industry and
+turning the worlds hosting their competitions into bustling hubs of
+tourism, industry and under-the-counter gambling when the venue's
+rent-a-cops aren't looking.
+
+**So, all things considered, not an entirely terrible deal.**
+
+Especially for the
+populace of a remote planet that, after some two hundred years of
+isolation, resource scarcity and desperate struggle for survival,
+eventually saw its homeworld become the main theater of an
+interstellar conflict. After all, the cities are already in shambles
+and the planet’s only moon has been sliced in two - letting some
+offworlders set up shop and build a couple of stadiums could hardly
+make things any worse.
+
+_**Welcome to Fist World Volga!**_
+`;
+
+var descriptionTremerine = `
+**TREMERINE: SEA OF THE STARS**
+
+**Year of Release: November 13th 2021**
+
+**Platform: Itch.io**
+
+**The year is 2394**
+
+Nine years after the
+tide of violence wrought by the Great Colonial War has upset a
+century-old balance of power, humanity is once again entering an era
+of fragile peace.
+
+From the ashes of
+the old Avignon Empire, firestarter of the conflict, a federation of
+former vassal worlds has risen - denying the tantalizing offer of
+growth and prosperity under the ever-increasing reach of the United
+Systems Alliance in favor of long-awaited independence.
+
+**Welcome to the Tremerine Union.**
+
+You and your
+companions find yourselves in the midst of all this, sailing the sea
+of the stars on the same starship for one reason or another: on a
+mission, in search of profits, or simply looking for a good time.
+Each of you may come from a different background or work a different
+profession, but you will need to cooperate to get the job done.
+
+_**Who will you be in this galaxy of opportunities?**_
+`;
+
+var descriptionShardana = `
+
+**SHARDANA: HYDRO TRAIL GP**
+
+**Year of Release: Hopefully 2023**
+
+**Platform: Itch.io**
+
+**The year is 2395.**
+
+A decade after the
+war that shook the Outer Colonies and brought the Avignon Empire to
+its knees, the Confederation of Shardana, an idyllic ocean world in
+the Aurelia System, has finally returned to relative normalcy. As the
+hulks of orbital warships float along coastlines lined with rubble,
+it’s clear that the worst has come to pass - and now, the only way
+to go is foward.
+
+**Forward into an uncertain, if ever so hopeful, future.**
+
+Organized by the
+Deadalus Consortium, Hydro Trail GP is an all-new naval racing
+championship designed to bring new eyes to the otherwise niche
+Shardanian national sport - a way to show, through glamourous events
+and cutting edge technology, that Shardana is prepared for the
+future.
+
+**Six heats, five teams, one champion.**
+
+As engines and
+rivalries alike flare up, the eyes of the Outer Colonies turn from
+Volga’s biomecha brawling to the sparkling blue waters of the Pearl
+of Aurelia. The starting grid is waiting, can you take the heat?
+
+_**Fasten your seatbelts and link up your drinking cable for Hydro Trail GP!**_
+`;
+
+var descriptionSirius = `
+**SIRIUS: TREACHEROUS REACHES**
+
+**Year of Release: [TBA]**
+
+**Platform: Itch.io**
+
+**The year is 2397.**
+
+Lightyears away from
+the blossoming Cosmic East, the sea of stars has become playing field
+to an orbital shadow war. The prize? The allegiance of the
+Sovereignty of Sirius - and the riches it can to offer to its new
+masters.
+
+**A shining gem of the void, torn apart from the inside.**
+
+The inhabitants of
+the Sirius System, from the highest-standing Kshatriya Admiral to the
+most destitute Shudra miner, have found themselves tangled into a
+silent civil war seeded by neighboring superpowers. All it takes is
+one “Blackout Mine”: paranoia and Caste-based grudges will do the
+rest.
+
+**In the shadow of the stars, no one can hear you scheme.**
+
+Life support systems
+are down to the bare minimum, maneuvering thrusters are dead and long
+range communications are fried: all you can do is listen to the pleas
+of the unfortunate souls stuck in this mess with you, trying to track
+their vessel through the radio waves and hoping they’re not using
+this time to get a bead on your position.
+
+_**Fill up your air tanks and pray you can survive Sirius: Treacherous Reaches.**_
+`;
---
+++ js/games.js
@@ -0,0 +1,79 @@
+/* -------------------------------------------------------------------------- */
+/*                               Simple carousel                              */
+/* -------------------------------------------------------------------------- */
+
+let currentItem = 0;
+let maxItem = 0;
+
+addItem("https://data-sparrow.itch.io/fist-world-volga", "/img/games/volga.png", descriptionVolga, true);
+addItem("https://data-sparrow.itch.io/tremerine-sea-of-the-stars", "/img/games/tremerine.png", descriptionTremerine);
+addItem("/html/wip.html", "/img/games/shardana.png", descriptionShardana);
+addItem("/html/wip.html", "/img/games/sirius.png", descriptionSirius);
+
+showItem(0);
+
+resizeGame();
+
+$("#button-left").on("click", function() {
+    moveItems(-1);
+});
+
+$("#button-right").on("click", function() {
+    moveItems(1);
+});
+
+$( window ).on("resize", function() {
+    resizeGame();
+});
+
+function resizeGame() {
+    $("#carousel").css("height", window.innerWidth / 6);
+    $("#carousel").css("min-height", window.innerWidth / 6);
+    $("#carousel").css("max-height", window.innerWidth / 6);
+}
+
+function moveItems(dir) {
+    currentItem += dir;
+    if (currentItem >= maxItem) currentItem = 0;
+    if (currentItem < 0) currentItem = maxItem - 1;
+    showItem(currentItem);
+}
+
+function showItem(num) {
+    $("#gamebox > *").hide();
+    $("#descbox > *").hide();
+    
+    $(`#gamebox > *:eq(${num})`).show();
+    $(`#descbox > *:eq(${num})`).show();
+}
+
+/**
+ * Adds item to GameBox
+ * @param {String} link Link to game
+ * @param {String} imagepath Path to game image
+ * @param {String} gamedescr Markdown of game description
+ */
+function addItem(link, imagepath, gamedescr, grayscale = false) {
+    let $elem = $(`
+    <img class="gameimg ${grayscale ? "img-bw":""}" src="${imagepath}"/>
+    `);
+
+    let $desc;
+    if (link == "/html/wip.html") {
+        $desc = $(`
+        <div class="gamedesc pjs-markdown">
+        <p><a style="color:red;">TBA</a></p>
+        ${gamedescr}
+        </div>`);
+    } else {
+        $desc = $(`<div class="gamedesc pjs-markdown">
+        <p><a href="${link}">Buy Game</a></p>
+        ${gamedescr}
+        </div>`);
+    }
+
+    $("#gamebox").append($elem);
+    $("#descbox").append($desc);
+
+    maxItem++;
+}
