fee898ce8192d829a5b0d87f42c662d339c3b3ce
Alisa Lain <60989682+al1-ce@users.noreply.github.com>
2023-08-17 01:04:54 +0300

    Updated contact and games and release

--- js/games-descriptions.js
+++ js/games-descriptions.js
@@ -12,32 +12,29 @@ var descriptionVolga = `
 <hr style="height: 2px; background-color: black; border: none;">
 <div style="min-height: 0.5em"></div>
 
-**The year is 2391.**
+_**The year is 2391_**
 
-Mankind has long expanded into the great starry void. 
-War after war, its path across outer space was paved by the flesh and bone of the fallen, 
-countless in number. Even now, at the dawn of newfound peace, 
-the echoes of battles to come ring in the ears of the survivors, 
-a grim reminder that all that lives is shackled to the unbreakable chains of history…
+Mankind has long expanded into the great starry void. War after war, its path across outer space was paved by the flesh and bone of the fallen, countless in number. Even now, at the dawn of newfound peace, the echoes of battles to come ring in the ears of the survivors, a grim reminder that all that lives is shackled to the unbreakable chains of history…
 
-**...but hey, at least we've got giant robot arenas now.**
+_**...but hey, at least we've got giant robot arenas now._**
 
-Scattered across the Outer Colonies under the ownership of Galactica Sports Entertainment, 
-"Fist World" combat circuits are a staple of recreational Battleframe combat, 
-turning the worlds hosting them into bustling hubs of tourism, 
-industry and under-the-counter gambling when the venue's rent-a-cops aren't looking.
+Scattered across the Outer Colonies under the ownership of Galactica Sports Entertainment, "Fist World" combat circuits are a staple of recreational Battleframe combat, turning the worlds hosting them into bustling hubs of tourism, industry and under-the-counter gambling when the venue's rent-a-cops aren't looking. 
 
-**So, all things considered, not an entirely terrible deal.**
+_**So, all things considered, not a terrible deal._**
 
-Especially for the populace of a remote planet that, 
-after some two hundred years of isolation, 
-resource scarcity and desperate struggle for survival, 
-eventually saw its homeworld become the main theater of an interstellar conflict. 
-After all, the cities are already in shambles and the planet's only moon 
-has been sliced in two - letting some offworlders build a couple of stadiums 
-could hardly make things any worse.
+Especially for the populace of a remote planet that, after some two hundred years of isolation, resource scarcity and desperate struggle for survival, eventually saw its homeworld become the main theater of an interstellar conflict. After all, the cities are already in shambles and the planet's only moon has been sliced in two - letting some offworlders build a couple of stadiums could hardly make things any worse. 
 
-_**Welcome to Fist World Volga!**_
+_**Welcome to Fist World Volga!_**
+
+Fist World Volga is a 1v1 "fighting board game", mixing the grid-based tactics of classic wargames like Battletech with the mechanical depth and unrelenting kineticism of arcade fighting games like Cyberbots: Full Metal Madness, Guilty Gear and...Zero Divide?
+
+Step into the cockpit of the Divjak MKII Sporting Bioframe and duke it out in the squared circle as one of eight different Frame Configurations, each armed with a variety of dubiously accurate guns and flashy melee weapons - from plain old rocket-powered fists to giant gunblades! 
+
+Learn your frame's strengths and master its matchups: the general ruleset is fairly lightweight, so it all comes down to knowing your tools and denying the opponent the chance to use theirs. 
+
+That, or going full gorilla and hoping your next raw heavy is the right one. 
+
+_**After all, no play is a bad play if it worked._**
 `;
 
 var descriptionTremerine = `
@@ -53,29 +50,23 @@ var descriptionTremerine = `
 <hr style="height: 2px; background-color: black; border: none;">
 <div style="min-height: 0.5em"></div>
 
-**The year is 2394**
+_**The year is 2394_**
 
-Seven years after the
-tide of violence wrought by the Great Colonial War has upset a
-century-old balance of power, humanity is once again entering an era
-of fragile peace.
+Seven years after the tide of violence wrought by the Great Colonial War upset a century-old balance of power, humanity is once again entering an era of fragile peace. From the ashes of the old Avignon Empire, firestarter of the conflict, a federation of former vassal worlds has risen - denying the tantalizing offer of growth and prosperity under the ever-increasing reach of the United Systems Alliance in favor of long-awaited independence.
 
-From the ashes of
-the old Avignon Empire, firestarter of the conflict, a federation of
-former vassal worlds has risen - denying the tantalizing offer of
-growth and prosperity under the ever-increasing reach of the United
-Systems Alliance in favor of long-awaited independence.
+_**Welcome to the Tremerine Union._**
 
-**Welcome to the Tremerine Union.**
+Built on the principles of "Safety, Equality and Dignity" and spanning nine worlds over four star systems, the Tremerine Union has since established itself as a respectably powerful player on the galactic chessboard - something of a middle ground between the Alliance's gossamer state and the Terran Protectorate's more authoritarian rulings. 
 
-You and your
-companions find yourselves in the midst of all this, sailing the sea
-of the stars on the same starship for one reason or another: on a
-mission, in search of profits, or simply looking for a good time.
-Each of you may come from a different background or work a different
-profession, but you will need to cooperate to get the job done.
+Focused on building over the decadence and devastation left in the wake of Avignon's fall, the Union has been working tirelessly to secure the great spaceways and the latest colonies discovered by the empire's Expanse Initiative, which in the meantime had become safe havens for surviving imperial remnants.
 
-_**Who will you be in this galaxy of opportunities?**_
+This effort would soon lead to the establishment of a national self-defense fleet, doing away with the "opportunity-based" mercenary army model taken from the Alliance in the early, turbulent years of reunification. This fleet now patrols the sea of stars, on the lookout for anyone daring enough to challenge this new peace.
+
+_**Which is where you come in._**
+
+You and your companions find yourselves in the midst of all this, sailing the sea of the stars on the same starship for one reason or another: on a mission, in search of profits, or simply looking for a good time. Each of you may come from a different background or work a different profession, but you will need to cooperate to get the job done.
+
+_**Who will you be in this galaxy of opportunities?_**
 `;
 
 var descriptionShardana = `
@@ -93,31 +84,33 @@ var descriptionShardana = `
 <hr style="height: 2px; background-color: black; border: none;">
 <div style="min-height: 0.5em"></div>
 
-**The year is 2395.**
+_**The year is 2395_**
 
-A decade after the
-war that shook the Outer Colonies and brought the Avignon Empire to
-its knees, the Confederation of Shardana, an idyllic ocean world in
-the Aurelia System, has finally returned to relative normalcy. As the
-hulks of orbital warships float along coastlines lined with rubble,
-it’s clear that the worst has come to pass - and now, the only way
-to go is foward.
+A decade after the war that shook the Outer Colonies and brought the Avignon Empire to its knees, the Confederation of Shardana, an idyllic ocean world in the Aurelia System, has finally returned to relative normalcy. As the hulks of orbital warships float along coastlines lined with rubble, it’s clear that the worst has come to pass - and now, the only way to go is foward.
 
-**Forward into an uncertain, if ever so hopeful, future.**
+_**Forward into an uncertain, if ever so hopeful, future._**
 
 Organized by the Deadalus Consortium, an association of local companies aiming to "relaunch" the planet's economy, Hydro Trail GP is an all-new naval racing championship designed to "bring offworld engagement" to the otherwise niche Shardanian national sport. A way to show, through glamourous racing events and cutting edge technology, that Shardana is prepared for the future.
 
-**The eyes of the galaxy turn as one, craving entertainment.**
+_**The eyes of the galaxy turn as one, craving entertainment._**
 
 Recruited from the ranks of the Consortium itself, five racing teams are taking part in the inaugural season - bringing with them old rivalries and setting the stage for whole new ones. And so, as the lights go green and jet engines roar awake, trillions of minds suddenly find themselves in tune - a single sentence flashing forth in that magical split second before all hell breaks lose on track.
 
-_**For the love of God, please don't mess it up.**_
+_**For the love of God, please don't mess it up._**
+
+Shardana: Hydro Trail GP is a 4-player nautical racing board game inspired by WipEout, Rapid Racer and actual powerboat racing - bringing a mix of high-octane arcade action and high-stakes precision maneuvers to the comfort of a rules-light "racing tactics" system. 
+
+Each player takes control of a BXS Series Racing Chassis, all designed for a particular driving style and armed with a unique Equipment Piece to turn the odds in its favor - whether it's simple Naval Airbrakes or Self-Repair Nanites.
+
+By default, the game also comes with six courses through which said chasses can be raced, all of which are easily set up thanks to a modular "Marker Buoy" system - mirroring the way real powerboat competitions set up their courses. Thanks to this same modularity however, players can just as easily build their own tracks - a process facilitated by the Custom Venue Profiles available in the game's free Companion Booklet.
+
+_**Fasten your seatbelts and link up your drink for Hydro Trail GP!_**
 `;
 
 var descriptionSirius = `
 **SIRIUS: TREACHEROUS REACHES**
 
-**Release Date: Autumn 2023**
+**Release Date: 2024**
 
 **Media Type: Social Deduction Board Game**
 
@@ -155,4 +148,5 @@ their vessel through the radio waves and hoping they’re not using
 this time to get a bead on your position.
 
 _**Fill up your air tanks and pray you can survive Sirius: Treacherous Reaches.**_
+<img src="/img/games/amogus.png" style="position:absolute;opacity:0.03;top:4em;right:1em;height:40px;"></img>
 `;
